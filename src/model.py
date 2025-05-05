0e1eef96adb3445ea7d2a6269a6a51ce06979a11
farshad7634 <farshad.z7634@gmail.com>
2025-05-05 23:53:00 +0330

    Non

---
+++ src/model.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class BreastCancerClassifier(nn.Module):
+    def __init__(self, num_classes=3):
+        super(BreastCancerClassifier, self).__init__()
+
+        # Load ResNet18 pretrained on ImageNet
+        self.model = models.resnet18(pretrained=True)
+
+        # Modify the first convolution layer to accept 1-channel grayscale input
+        self.model.conv1 = nn.Conv2d(
+            in_channels=1,  # grayscale
+            out_channels=64,
+            kernel_size=7,
+            stride=2,
+            padding=3,
+            bias=False
+        )
+
+        # Replace the final fully connected layer for our 3 classes
+        self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
+
+    def forward(self, x):
+        return self.model(x)
